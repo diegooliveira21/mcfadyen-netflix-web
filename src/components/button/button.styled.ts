04028d0ebe9579b1cf22aae3ab360d428e2ed7f8
Diego Oliveira <yuv8AL22HJ9ZmvT>
2022-06-21 22:09:47 -0300

    first commit

---
+++ src/components/button/button.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: #FFF;
+  width: 100%;
+  padding: 9px;
+  margin: 0 0 22px;
+  border-radius: 5px;
+  background-color: #E31A13;
+  border: 0;
+`
